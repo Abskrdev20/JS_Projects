f1391b3e607f9ff0af1c6f4048403d99322de2e5
Abhishek Kumar <absk.devopsmastero@gmail.com>
2025-08-03 07:34:03 +0000

    Add Project 05 : Unlimited_Colors

---
+++ P05_Unlimited_Colors/app.js
@@ -0,0 +1,25 @@
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+let intervalID;
+const startChangingColor = function () {
+  const changeBgColor = ()=>{
+    document.body.style.backgroundColor = randomColor();
+  }
+  if(!intervalID){
+    intervalID = setInterval(changeBgColor,1000)
+  }
+  };
+const stopChangingColor = function () {
+  clearInterval(intervalID)
+  intervalID = null;
+};
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
